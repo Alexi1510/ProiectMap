edfee94525b46869f51b880390780243666d5f88
Alexi1510 <ienciualex@yahoo.com>
2023-01-12 14:55:30 +0200

    AM adaugat functia print solution si place queen unde unde print solution afiseaza toate variantele in care regina a fost plasata pe tabla de sah iar place queen este functia unde se parcurge si se plaseaza regina pe tabla de sah

--- proiect.c
+++ proiect.c
@@ -15,6 +15,36 @@ bool valid(int queen_number) {
   return true;
 }
 
+void print_solution() {
+  static int solutions = 0;
+  printf("Solution %d:\n", ++solutions);
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      if (sol[i] == j) {
+        printf("Q ");
+      } else {
+        printf(". ");
+      }
+    }
+    printf("\n");
+  }
+  printf("\n");
+}
+
+void place_queen(int queen_number) {
+  if (queen_number == N) {
+    print_solution();
+    return;
+  }
+  for (int row = 0; row < N; row++) {
+    sol[queen_number] = row;
+    if (valid(queen_number)) {
+      place_queen(queen_number + 1);
+    }
+  }
+}
+
+
 int main()
 {
     return 0;
